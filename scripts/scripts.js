23d9dc2242d46e97c399f4ab06766f695fa29671
Razberrymc <55071516+Razberrymc@users.noreply.github.com>
2019-11-01 17:04:14 -0400

    Update scripts.js

--- scripts/scripts.js
+++ scripts/scripts.js
@@ -14,22 +14,6 @@ else {
 }
 
 
-
-/*const relax = document.querySelector('#relax');
-
-relax.onmouseover = () => {
-  if (relax.getAttribute("src") ==="images/relax_breathe.jpg") {
-
-    relax.setAttribute("src","images/and_breathe.jpg");
-
-
-  }
-else {
-  relax.setAttribute("src","images/relax_breathe.jpg");
-}
-
-}*/
-
 relax.onmouseover = () => {
   relax.setAttribute("src","images/and_breathe.jpg");
 }
